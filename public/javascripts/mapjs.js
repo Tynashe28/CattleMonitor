2f3a441280af7d280ab85d01df726057a9497655
Tynashe28 <n01519975j@students.nust.ac.zw>
2021-09-17 23:38:47 +0200

    added ma sinh eku drawer

--- public/javascripts/mapjs.js
+++ public/javascripts/mapjs.js
@@ -10,4 +10,25 @@ function initMap() {
     position: cattle,
     map: map,
   });
+  const drawingManager = new google.maps.drawing.DrawingManager({
+    
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.POLYGON,
+      ],
+    },
+    
+    circleOptions: {
+      fillColor: "#ffff00",
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1,
+    },
+  });
+
+  drawingManager.setMap(map);
 }
