d2b5a9c73530afcf6f632c488b350e80a987d62c
Tinashe Mabika <n01519975j@students.nust.ac.zw>
2021-10-16 21:01:56 +0200

    check req

--- routes/api.js
+++ routes/api.js
@@ -10,10 +10,10 @@ const SensorData = require('../Models/SensorsData');
 router.post('/v1/fieldParams',async(req,res)=>{
    
     const cowData = {
-        lat : req.body.l,
-        lng: req.body.g,
-        temperature :req.body.t,
-        pulse:0
+        lat : req.body.latitude,
+        lng: req.body.longitude,
+        temperature :req.body.temperature,
+        pulse:req.body.pulseRate
     }
     const sendData = new SensorData(cowData);
     await sendData.save().catch(err=>{});
